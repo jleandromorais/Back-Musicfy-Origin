47b8d9002c2b55c6023acd0ecff386272d545bf1
jleandromorais <leandromorais12345@outlook.com>
2025-07-09 17:42:26 -0300

    Adicionanco docker para deploy

--- src/main/java/Musicfy/MusicfyOrigin/Product/confing/CorsConfig.java
+++ src/main/java/Musicfy/MusicfyOrigin/Product/confing/CorsConfig.java
@@ -1,23 +1,27 @@
 package Musicfy.MusicfyOrigin.Product.confing;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://musicfy-558s99apl-jleandromorais-projects.vercel.app"
 
-
-//ola mundo
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // URL do seu frontend
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
